4e45854fe961d6a4e34432a9647a680f7b061721
YeWeon <119658218+YeWeon@users.noreply.github.com>
2024-05-06 15:46:04 +0900

    Update 단지번호붙이기.py

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,40 @@
+from collections import deque 
+
 n = int(input())
 
 maps = []
 for i in range(n):
     maps.append(list(map(int, input())))
 
-def dfs(x, y):
-    global count
-    if 0 > x or x >= n or y < 0 or y >= len(maps):
-        return False
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+visited = [[False] * len(maps) for i in range(n)]
 
-    if maps[x][y] == 1:
-        maps[x][y] = 0
-        count += 1
-        dfs(x-1, y)
-        dfs(x, y-1)
-        dfs(x + 1, y)
-        dfs(x, y + 1)
-        return True
-
-    return False 
+def bfs(x, y):
+    q = deque([(x, y)])
+    count = 1
+    while q:
+        x, y = q.popleft() 
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < len(maps):
+                if maps[nx][ny] == 1 and not visited[nx][ny]:
+                    count += 1 
+                    q.append((nx, ny))
+                    visited[nx][ny] = True
+    return count 
 
 res = 0
-house = []
+count = []
 for i in range(n):
     for j in range(len(maps)):
-         count = 0
-         if dfs(i, j):
-             res += 1
-             house.append(count)
+        if maps[i][j] == 1 and not visited[i][j]:
+            visited[i][j] = True 
+            count.append(bfs(i, j))
+            res += 1
 
 print(res)
-house.sort()
+count = sorted(count)
 for i in range(res):
-    print(house[i])
+    print(count[i])
